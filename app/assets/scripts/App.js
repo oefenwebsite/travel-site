47d880faa6a72ab34b0ef70fc901224d3248ad53
Geena <frankrijkfriesland@gmail.com>
2020-08-02 16:31:29 +0200

    cursus 50 gedaan

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,4 @@
 import '../styles/styles.css'
-import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
